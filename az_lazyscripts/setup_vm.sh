92cce8aaecb9ca246988ad459f80968f5cc42830
ceej64 <32303858+ceej64@users.noreply.github.com>
2019-01-31 20:16:51 +0000

    Add files via upload

---
+++ az_lazyscripts/setup_vm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+sudo apt-get -y update       
+#don't do apt-get upgrade because it does not work with AWS
+sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
+git clone https://github.com/ceej64/xmr-stak.git
+
+cd xmr-stak
+chmod u+x az_lazyscripts/compile_and_config.sh
+chmod u+x az_lazyscripts/run_xmr_stak.pl
+chmod u+x az_lazyscripts/create_cpu_config.sh
+chmod u+x az_lazyscripts/create_pool_config.sh
+az_lazyscripts/compile_and_config.sh
+
+
+
+
+
+
+
